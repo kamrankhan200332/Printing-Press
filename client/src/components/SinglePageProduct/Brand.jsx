c0445c3bcc5b89cd96fff2dbc7ed15d6ca39a2a8
Kamran <kamrankhan815.com@gmail.com>
2025-06-16 15:01:06 +0500

    complete the single product page

--- client/src/components/SinglePageProduct/Brand.jsx
+++ client/src/components/SinglePageProduct/Brand.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import brandImg from "../../assets/images/brand.png";
 
 const Brand = () => {
   return (
     <div>
-      Brand
+      <div className="space-y-1">
+        <h1 className="text-2xl sm:text-3xl font-bold text-gray-700">Brand</h1>
+        <p className="text-xl font-bold text-gray-700">
+          Printing Press Online
+        </p>
+        <p>
+          Printing Press in Rawalpindi Provides Online Printing Services in
+          Pakistan
+        </p>
+        <div className="img">
+          <img src={brandImg} alt="brandImage" />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Brand;
--- client/src/components/SinglePageProduct/Review.jsx
+++ client/src/components/SinglePageProduct/Review.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Review = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      Reviews
+      <div className="space-y-3 sm:space-y-5 ">
+        <h1 className="text-2xl sm:text-3xl font-bold text-gray-700">Reviews</h1>
+        <div>
+          <p>There are no reviews yet.</p>
+          <p>Be the first to review “Sticker Printing”</p>
+          <p>
+            You must be{" "}
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => navigate("/login")}
+            >
+              logged in
+            </span>{" "}
+            to post a review.
+          </p>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Review;
